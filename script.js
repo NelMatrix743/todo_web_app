eb43bc12ef0eadc78728f51f88482bcee03e4b62
NelMatrix743 <nelmatrix155@gmail.com>
2025-09-12 20:36:34 +0000

    feat: map DOM nodes to respective JavaScript objects

---
+++ script.js
@@ -0,0 +1,12 @@
+/**
+ *  SOURCE CODE VERSION: 1.0.0
+ *  PROGRAMMER: NELSON CHIDI (NELMATRIX)
+ *  DATE-TIME WRITTEN: SEPT 12, 2025; 8:20PM
+ */
+
+
+// mapped elements
+
+const inputBox = document.getElementById("input-box");
+const taskListContainer = document.getElementById("task-list-container");
+const taskInsertBtn = document.getElementById("insert-task-button");
